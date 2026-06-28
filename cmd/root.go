3e814c79641f26bca8a960ff7665fec904db852e
Joe Corall <joe@libops.io>
2026-06-28 22:53:04 +0000

    Move rollout commands into plugin metadata

--- cmd/root.go
+++ cmd/root.go
@@ -42,8 +42,12 @@ func createDefinition() plugin.CreateSpec {
 		DockerComposeUp: []string{
 			"docker compose up --remove-orphans -d",
 		},
-		DockerComposeDown:    []string{"docker compose down"},
-		DockerComposeRollout: []string{"./scripts/rollout.sh"},
+		DockerComposeDown: []string{"docker compose down"},
+		DockerComposeRollout: []string{
+			"docker compose pull --ignore-buildable --quiet || docker compose pull --ignore-buildable || true",
+			"./scripts/init.sh",
+			"docker compose up --remove-orphans --wait --pull missing --quiet-pull -d",
+		},
 	}
 }
 
